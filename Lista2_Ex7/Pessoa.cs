872a441fd60ba1c4fd0e0e23e6e2a63c1bf1a8f7
Bianca Ribeiro <132418587+biancafdribeiro@users.noreply.github.com>
2023-08-31 01:37:24 -0300

    Exercício 7

---
+++ Lista2_Ex7/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2_Ex7
+{
+    public class Pessoa
+    {
+        public static int TotalPessoas { get; private set; } //evita que a propriedade seja mudada
+        public static int SomarPessoas()
+        {
+            TotalPessoas++;
+            return TotalPessoas;
+        }
+    }
+}
---
+++ Lista2_Ex7/Program.cs
@@ -0,0 +1,25 @@
+﻿using Lista2_Ex7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Construa uma classe chamada Pessoa em C# com uma propriedade estática chamada TotalPessoas 
+ que guarda o número total de instâncias da classe criadas. O valor de TotalPessoas deve ser 
+ atualizado sempre que uma nova instância de Pessoa for criada.*/
+
+namespace Lista2_Ex7
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa.SomarPessoas();
+            Pessoa.SomarPessoas(); //chamando 2 vezes
+
+            Console.WriteLine("O valor atual é {0}", Pessoa.TotalPessoas);
+            Console.ReadKey();
+        }
+    }
+}
